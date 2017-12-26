2311601600a3da66b95861714929c0f5c2685903
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2017-12-26 23:52:35 +0530

    update for pop to old pg

--- first/first/Purple.swift
+++ first/first/Purple.swift
@@ -25,14 +25,10 @@ class Purple: UIViewController {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    @IBAction func navarraymove(_ sender: Any) {
+        var x = self.navigationController?.viewControllers[0] as! ViewController
+        self.navigationController?.popToViewController(x, animated: true)    }
+   
 
 }
